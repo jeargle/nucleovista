191e67f353148d88a49e9715086729db44cfb066
John Eargle <jeargle@gmail.com>
2021-12-30 17:28:39 -0600

    added initial file stubs

---
+++ test/test.js
@@ -0,0 +1,12 @@
+/* JSLint */
+/*global console: false, nucleovista: false, extend: false*/
+
+var test = test || {}
+
+test.testStart = function() {
+    'use strict'
+
+    console.log('\n\n***** start Test *****\n')
+}
+
+test.testStart()
